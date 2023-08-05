f2aded8500141ba647d40df40afee386107aac9c
gatman5227 <gatman5227@gmail.com>
2023-08-05 01:25:34 -0400

    created intial files

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Landing page</title>
+    <link rel="stylesheet" href="css/index_css.css">
 </head>
 <body>
-    
+    <h1>Test message</h1>
+    <h2>This will be blank for now</h2>
 </body>
 </html>
